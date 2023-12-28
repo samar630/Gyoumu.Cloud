0688ec7fa0e16d7abc0fc1c3eb66a21b06f9ce7e
SamarQaddour <samarqaddour051@gmail.com>
2023-12-28 11:42:23 -0800

    sort code

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,6 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, ViewEncapsulation, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormGroupDirective, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-import { FloatLabelType } from '@angular/material/form-field';
-import { Router } from '@angular/router';
-import { loginInterface } from 'src/app/moduleLogin/login.interface';
+
+import { Component} from '@angular/core';
+
 
 
 @Component({
